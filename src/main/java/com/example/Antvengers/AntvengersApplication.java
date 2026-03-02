503885a6b2e0876e9e772c8a2885456b3fa938ca
desarrollo <desarrollo@academicos.cesde.edu.co>
2026-03-02 07:18:25 -0500

    feature - se crea proyecto

---
+++ src/main/java/com/example/Antvengers/AntvengersApplication.java
@@ -0,0 +1,13 @@
+package com.example.Antvengers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntvengersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntvengersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Antvengers/AntvengersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Antvengers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntvengersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
